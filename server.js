14d35fbb9b0425539621e1d2b246b09e71d6b820
govindh7866 <workwithgovind229@gmail.com>
2026-08-04 20:12:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin.html"));
 });
 
 const PF = path.join(__dirname, "products.json");
